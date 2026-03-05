c9681fde7aab2622f518fde721b9b44e8a78c8f0
Devanik <162272415+Devanik21@users.noreply.github.com>
2026-03-05 19:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def set_png_as_page_bg(bin_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Set the local background image
-if os.path.exists("Aesthetic_2.png"):
-    set_png_as_page_bg("Aesthetic_2.png")
+if os.path.exists("Aesthetic_3.png"):
+    set_png_as_page_bg("Aesthetic_3.png")
 
 # Custom CSS for magical, dreamy, transparent pixel aesthetic
 st.markdown("""
